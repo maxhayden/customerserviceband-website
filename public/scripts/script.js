45356852ff86d910adfdad85ea27e25582ad8a25
maxhayden <maxphayden@gmail.com>
2025-03-15 11:47:30 -0300

    updated nav bar

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,23 +1,36 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 document.getElementById("toggleScroll").addEventListener("click", function () {
-        document.getElementById("scrollContainer").classList.toggle("active");
-        document.getElementById("toggleScroll").classList.toggle("active");
-        document.getElementById("home").classList.toggle("active");
-        document.getElementById("main-content").classList.toggle("opacity-50");
-    });
-    
+    document.getElementById("scrollContainer").classList.toggle("active");
+    document.getElementById("toggleScroll").classList.toggle("active");
+    document.getElementById("home").classList.toggle("active");
+
+});
+
+document.getElementById("home").addEventListener("click", function () {
+    document.getElementById("scrollContainer").classList.remove("active");
+    document.getElementById("toggleScroll").classList.remove("active");
+    document.getElementById("home").classList.add("active");
+
+});
 
 document.getElementById("toggleScrollOff").addEventListener("click", function () {
-  document.getElementById("scrollContainer").classList.toggle("active");
-  document.getElementById("toggleScroll").classList.toggle("active");
-  document.getElementById("home").classList.toggle("active");
-  document.getElementById("main-content").classList.toggle("opacity-50");
+    document.getElementById("scrollContainer").classList.toggle("active");
+    document.getElementById("toggleScroll").classList.toggle("active");
+    document.getElementById("home").classList.toggle("active");
 });
 
-document.getElementById("toggleScroll").addEventListener("keydown", function(event) {
-  if (event.key === "Enter" || event.key === " ") {
-      event.preventDefault(); 
-      this.click();
-  }
+document.getElementById("toggleScroll").addEventListener("keydown", function (event) {
+    if (event.key === "Enter" || event.key === " ") {
+        event.preventDefault();
+        this.click();
+    }
 });
+
+document.getElementById("home").addEventListener("keydown", function (event) {
+    if (event.key === "Enter" || event.key === " ") {
+        event.preventDefault();
+        this.click();
+    }
+});
+
